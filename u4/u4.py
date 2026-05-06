8ee4c94551027252f9085a001f24d9b44be28f2a
bayertom <bayertom@users.noreply.github.com>
2026-05-06 10:14:51 +0200

    cv11

--- u4/u4.py
+++ u4/u4.py
@@ -1,16 +1,42 @@
 from numpy import *
 from pyproj import *
 
-def project(proj_name,R_z, u, v, u0):
+def project(proj_name, R_z, lat, lon, lat0):
     # Create new projection
-    my_proj =  Proj(proj=proj_name, R=R_z, lat_1=u0)
+    my_proj =  Proj(proj=proj_name, R=R_z, lat_1 = lat0)
 
     # Project point calculation
-    [X,Y] = my_proj(v,u)
+    [X,Y] = my_proj(lon, lat)
 
     #Distortions
-    dist = my_proj.get_factors(v, u)
+    dist = my_proj.get_factors(lon, lat)
     a = dist.tissot_semimajor
     b = dist.tissot_semiminor
 
-    return X, Y, a, b
+    return X, Y, a, b 
+
+#Define projection
+proj_name = "sinu"
+R = 6380000
+lat0 = 10
+
+#Define grid
+lat_min = -80
+lat_max = 80
+lon_min = -180
+lon_max = 180
+nlat = 10
+nlon = 10
+
+
+#Create intervals
+lat = linspace(lat_min, lat_min, nlat)
+lon = linspace(lon_min, lon_min, nlon)
+
+#Create  meshgrid
+latg, long = meshgrid(lat, lon)
+
+#Project meshgrid
+X, Y, a, b = project(proj_name, R, latg, long, lat0)
+
+print(X)
